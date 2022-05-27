bd65e08cfdb074b1a8056c69bb468602a625254d
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-27 09:48:34 +0530

    learnt about how to handle promices in javascript

---
+++ node_mongodb/handle_promises.js
@@ -0,0 +1,47 @@
+const {MongoClient} = require('mongodb');
+
+const url = 'mongodb://0.0.0.0:27017';
+const dbName = 'Test';
+const collectionName = 'Document';
+
+const client = new MongoClient(url);
+// console.log(client);
+
+// M-1 => Handling promises by using async and await.
+
+// async function getData(){
+//     const connect = await client.connect();
+//     console.log('connected to local server');
+//     const db = connect.db(dbName);
+//     const collection = db.collection(collectionName);
+//     const data = await collection.find().toArray();
+//     console.log("Data is =>> ", data);
+//     // console.log(`Data is =>> ${data}`);
+// }
+
+// getData();
+
+// M-2 
+client.connect().then( (connection) => {
+    console.log("1");
+    return connection.db(dbName);
+}).then( (db) => {
+    console.log("2");
+    return db.collection(collectionName);
+}).then( (collection) => {
+    console.log("3");
+    return collection.find().toArray();
+}).then( (data) => {
+    // console.log(data);
+    data.forEach( (item) => {
+        console.log(item);
+    })
+}).catch( (err) => {
+    console.log(err);
+}).finally( () =>{
+    console.log("done");
+})
+
+
+
+
